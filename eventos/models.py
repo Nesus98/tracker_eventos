57c02e800041be639232e88744b96e08d599d6b0
Nesus98 <jesusarroyo30.98@gmail.com>
2026-06-11 10:19:57 +0200

    tailwind added

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    nombre = models.CharField(max_length=100)
+    lugar = models.CharField(max_length=150)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Candidatura(models.Model):
+    ESTADOS = (
+        ('ENVIADA', 'Curriculum Enviado'),
+        ('ENTREVISTA', 'En proceso de entrevista'),
+        ('ACEPTADA', '¡Contratado!'),
+        ('RECHAZADA', 'Descartado'),
+    )
+
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='candidaturas')
+    puesto = models.CharField(max_length=150)
+    estado = models.CharField(
+        max_length=15,
+        choices=ESTADOS,
+        default='ENVIADA'
+    )
+    fecha_aplicacion = models.DateField(auto_now_add=True)
+    notas = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.puesto} - {self.evento.nombre} ({self.estado})"
--- eventos/views.py
+++ eventos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Evento, Candidatura
 
-# Create your views here.
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    return render(request, 'eventos/lista_eventos.html', {'eventos': eventos})
+
+def lista_candidaturas(request):
+    candidaturas = Candidatura.objects.all()
+    return render(request, 'eventos/lista_candidaturas.html', {'candidaturas': candidaturas})
